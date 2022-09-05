0164732c9997d1a3f3d610771e123232481546a9
facu-pal <facundo.palmieri.01@gmail.com>
2022-09-05 19:33:50 -0300

    ejercio c02 - estanteria

---
+++ Ejercicio C02 - La estantería/entidades/Estante.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entidades
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+        private Estante(int capacidad)
+        {
+            productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return productos;
+        }
+
+        public static string MostrarEstante (Estante estante)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Estante ubicado en: {estante.ubicacionEstante}");
+
+            foreach (Producto item in estante.productos)
+            {
+                if (!Object.ReferenceEquals(item, null))
+                {
+                    sb.AppendLine(Producto.MostrarProducto(item));
+                    sb.AppendLine("----------------------");
+
+                }
+            }
+
+
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Estante estante, Producto producto)
+        {
+
+            foreach (Producto item in estante.productos)
+            {
+                if (item == producto)
+                {
+                    return true;
+
+                }
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Estante estante, Producto producto)
+        {
+            return !(estante == producto);
+        }
+
+        public static bool operator +(Estante estante, Producto producto)
+        {
+
+            if (estante != producto)
+            {
+                for (int i = 0; i < estante.productos.Length; i++)
+                {
+                    if (estante.productos[i] is null)
+                    {
+                        estante.productos[i] = producto;
+                        return true;
+
+                    }
+                }
+            }
+
+            return false;
+        }
+        public static Estante operator -(Estante estante, Producto producto)
+        {
+
+            for (int i = 0; i < estante.productos.Length; i++)
+            {
+                if (estante == producto)
+                {
+                    estante.productos[i] = null;
+                    break;
+                }
+            }
+            return estante;
+
+        }
+    }
+}
---
+++ Ejercicio C02 - La estantería/entidades/Producto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entidades
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string codigoDeBarra, string marca, float precio)
+        {
+            this.codigoDeBarra = codigoDeBarra;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto producto)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"MARCA: {producto.marca} - codigo De Barra:{(string)producto} - precio {producto.precio}");
+
+            return sb.ToString();
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto produc1, Producto produc2)
+        {
+            if (!(produc1 is null || produc2 is null))
+            {
+                return produc1.marca == produc2.marca && produc1.codigoDeBarra == produc2.codigoDeBarra;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Producto produc1, Producto produc2)
+        {
+
+            return !(produc1 == produc2);
+        }
+
+        public static bool operator ==(Producto produc, string auxStr)
+        {
+            if (produc.codigoDeBarra == auxStr)
+            {
+                return true;
+            }
+            return produc.codigoDeBarra == auxStr;
+        }
+
+        public static bool operator !=(Producto produc1, string auxStr)
+        {
+
+            return !(produc1 == auxStr);
+        }
+
+
+    }
+
+}
